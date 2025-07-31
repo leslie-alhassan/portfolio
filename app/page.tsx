212623424acd99f2d1dd04f9f648ca1811d44896
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2025-07-31 14:53:42 -0400

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Leslie Al-Hassan</h1>
+      <p>software developer based in Toronto, Canada</p>
+
+      <p>
+        software developer @{' '}
+        <a
+          href='https://www.northone.com/'
+          target='_blank'
+        >
+          North One
+        </a>
+      </p>
+
+      <a
+        href=''
+        target='_blank'
+      >
+        resume
+      </a>
+    </div>
+  )
+}
